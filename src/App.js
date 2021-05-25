9a0ddddd1870d5f1b68aa6cf238ecbf9e5c26736
Phan Tung <tungpt.seven@gmail.com>
2021-05-25 16:45:03 +0700

    update app with sign-in page, include: - custom form-input and button - sign-in form - authen page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/header/header.component';
+import AuthenticationPage from './pages/authenticationPage/authenticationPage.component';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
@@ -9,9 +11,11 @@ import ShopPage from './pages/shop/shop.component';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={ShopPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={AuthenticationPage} />
       </Switch>
     </div>
   );
