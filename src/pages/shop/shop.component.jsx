40fcf5cbdafb1c545fae08daa1d0585a7217c016
Phan Tung <tungpt.seven@gmail.com>
2021-06-07 23:19:13 +0700

    complete routing with param

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,13 +2,18 @@ import React from 'react'
 import { Route } from 'react-router'
 
 import CollectionsOverview from '../../components/collection-overview/collection-overview.component'
+import CollectionPage from '../../pages/collectionPage/collectionPage.component'
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <CollectionsOverview />
-    {/* <Route exact path={`${match.path}`} component={CollectionsOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage}/> */}
-  </div>
-)
+
+const ShopPage = ({ match }) => {
+
+  return (
+
+    <div className='shop-page'>
+      <Route exact path={`${match.path}`} component={CollectionsOverview} />
+      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+    </div>
+  )
+}
 
 export default ShopPage
